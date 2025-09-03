6672421f71bab24a9729a0aa66fa00e22773da4a
Rohan-max43 <gitpractices@gmail.com>
2025-09-03 12:01:10 +0600

    add numpy and bug-fix file

---
+++ numpy_projects.py
@@ -0,0 +1,15 @@
+# git bug-fix
+import numpy as np
+
+arr_2d = np.array([[1,2,3],
+              [4,5,6]])
+print(arr_2d)
+
+arr_1d = np.array([1, 2, 3])
+print(arr_1d)
+
+arr_3d = np.array([
+    [[1, 2, 3],[4, 5, 6]],
+    [[7, 8, 9],[3, 2, 4]]
+])
+print(arr_3d)
